0113e0303837215cb7a40de56a5c75300e5a2e2b
Jonathan Persgården <persg013@umn.edu>
2020-05-17 11:51:59 +0200

    Glide: Added layout to AuthAcitity
    Used gilde to populate an imageview. The Glide instance is provided in the
    Dagger AppModule.

--- Android/app/src/main/java/com/example/daggerpractice/AuthActivity.java
+++ Android/app/src/main/java/com/example/daggerpractice/AuthActivity.java
@@ -1,8 +1,11 @@
 package com.example.daggerpractice;
 
+import android.graphics.drawable.Drawable;
 import android.util.Log;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.bumptech.glide.RequestManager;
 import dagger.android.support.DaggerAppCompatActivity;
 import javax.inject.Inject;
 
@@ -11,13 +14,21 @@ public class AuthActivity extends DaggerAppCompatActivity {
     private static final String TAG = "AuthActivity";
 
     @Inject
-    String someString;
+    Drawable mLogo;
+
+    @Inject
+    RequestManager mRequestManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+        setLogo();
+    }
 
-        Log.d(TAG, "onCreate: "+ someString);
+    private void setLogo(){
+        mRequestManager.
+                load(mLogo)
+                .into((ImageView) findViewById(R.id.login_logo));
     }
 }
--- Android/app/src/main/java/com/example/daggerpractice/di/AppModule.java
+++ Android/app/src/main/java/com/example/daggerpractice/di/AppModule.java
@@ -1,5 +1,12 @@
 package com.example.daggerpractice.di;
 
+import android.app.Application;
+import android.graphics.drawable.Drawable;
+import androidx.core.content.ContextCompat;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.daggerpractice.R;
 import dagger.Module;
 import dagger.Provides;
 
@@ -7,7 +14,20 @@ import dagger.Provides;
 public class AppModule {
 
     @Provides
-    static String someString() {
-        return "Injected string!!";
+    static RequestOptions provideRequestOptions() {
+        return RequestOptions
+                .placeholderOf(R.drawable.white_background)
+                .error(R.drawable.white_background);
+    }
+
+    @Provides
+    static RequestManager provideGlideInstance(Application application, RequestOptions requestOptions) {
+        return Glide.with(application)
+                .setDefaultRequestOptions(requestOptions);
+    }
+
+    @Provides
+    static Drawable provideAppDrawable(Application application) {
+        return ContextCompat.getDrawable(application, R.drawable.logo);
     }
 }
