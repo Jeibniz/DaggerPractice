4bed49e2776b616bc2bab44ca9d93f4150fd548c
Jonathan Persgården <persg013@umn.edu>
2020-05-17 21:57:30 +0200

    MainActivity Toolbar: Added a toolbar with a menu to MainActivity
    The menu contain a logout option. Clicking that will call logout in the
    session manager. This will trigger MainAnctivity to finish and AuthActivity
    to load.

--- Android/app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.java
+++ Android/app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.daggerpractice.ui.main;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
 import com.example.daggerpractice.BaseActivity;
@@ -15,5 +18,29 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Hello from Main", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.main_menu_logout:
+                onMenuLogoutClick();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onMenuLogoutClick() {
+        sessionManager.logOut();
     }
 }
